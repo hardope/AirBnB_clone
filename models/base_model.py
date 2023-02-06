6ef6a806577efda74362841200e48b8015965baf
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-02-06 09:13:48 +0100

    Enable Kwargs in base model initialization for deserialization

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,31 @@ class BaseModel:
      """This class will defines all common attributes/methods
      for other classes
      """
-     def __init__(self):
-          """Initialize Base class Model
+     def __init__(self, *args, **kwargs):
+          """Initialize Base class Model with provided information
+          Args:
+               args: it won't be used
+               kwargs: arguments for the constructor of the BaseModel
+          Attributes:
+               id: unique id generated
+               created_at: creation date
+               updated_at: updated date
           """
-          self.id = str(uuid.uuid4())
-          self.created_at = self.updated_at = datetime.now()
+          if kwargs:
+               for key, value in kwargs.items():
+                    if key == "created_at" or key == "updated_at":
+                         value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    if key != "__class__":
+                         setattr(self, key, value)
+               if "id" not in kwargs:
+                    self.id = str(uuid.uuid4())
+               if "created_at" not in kwargs:
+                    self.created_at = datetime.now()
+               if "updated_at" not in kwargs:
+                    self.updated_at = datetime.now()
+          else:
+               self.id = str(uuid.uuid4())
+               self.created_at = self.updated_at = datetime.now()
 
      def __str__(self):
           """String Representation of data in each object
